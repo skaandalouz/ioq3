89995db6a9f68d1b832e1a61e98584669daa1dc4
BidyBiddle <hsbiddle@gmail.com>
2021-09-26 19:00:26 +0200

    Raised server max client limit
    Give server admins the ability to host large game events such as knockouts/gungame/LMS.
    
    No reason to keep the old 32 q3 limit in UrT.

--- code/server/sv_snapshot.c
+++ code/server/sv_snapshot.c
@@ -367,8 +367,8 @@ static void SV_AddEntitiesVisibleFromPoint( vec3_t origin, clientSnapshot_t *fra
 		}
 		// entities can be flagged to be sent to a given mask of clients
 		if ( ent->r.svFlags & SVF_CLIENTMASK ) {
-			if (frame->ps.clientNum >= 32)
-				Com_Error( ERR_DROP, "SVF_CLIENTMASK: clientNum >= 32" );
+			if (frame->ps.clientNum >= 64)
+				Com_Error( ERR_DROP, "SVF_CLIENTMASK: clientNum >= 64" );
 			if (~ent->r.singleClient & (1 << frame->ps.clientNum))
 				continue;
 		}
